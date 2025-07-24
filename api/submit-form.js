57a8bd5333bdd8d2f440f4ade8f0cbb880c15712
jamesredd64 <99228997+jamesredd64@users.noreply.github.com>
2025-07-24 00:53:34 -0400

    fix: collection name wrong

--- api/submit-form.js
+++ api/submit-form.js
@@ -12,11 +12,11 @@ module.exports = async (req, res) => {
   // if (!isLocal && !tokenStore.isValid(token)) {
   //   return res.status(403).json({ success: false, error: 'Invalid or expired token' });
   // }
-  
+
 
   try {
     const db = await getDb();
-    const submissions = db.collection('formSubmissions');
+    const submissions = db.collection('formsubmissions');
 
     const result = await submissions.insertOne({
       ...formData,
